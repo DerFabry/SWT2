08eb793b3a94e5eb8fbc002dc11be6ba6c7581c3
Stephan <Stephan@DerGeraet>
2016-06-15 00:35:40 +0200

    Refactoring, korrektes Nesting

--- src/gui/GameFrame.java
+++ src/gui/GameFrame.java
@@ -12,81 +12,21 @@ import javax.swing.SpringLayout;
  */
 public class GameFrame extends JFrame {
 	private GamePanel mainPanel;
-	private HandPanel handPanel;
-	private BattlefieldPanel bfPanel;
 	private SpringLayout sl_mainPanel, sl_bfPanel;
-	private PlayerInfoPanel opponentInfo1, opponentInfo2, opponentInfo3, opponentInfo4;
-	private OwnInfoPanel ownInfo;
-	private int opponentInfoPanelWidth, opponentInfoPanelHeight;
 
 	public GameFrame() {
 		mainPanel = new GamePanel();
-		handPanel = new HandPanel();
-		bfPanel = new BattlefieldPanel();
-
-		opponentInfo1 = new PlayerInfoPanel();
-		opponentInfo2 = new PlayerInfoPanel();
-		opponentInfo3 = new PlayerInfoPanel();
-		opponentInfo4 = new PlayerInfoPanel();
-
-		ownInfo = new OwnInfoPanel();
 
 		sl_mainPanel = (SpringLayout) mainPanel.getLayout();
-		sl_bfPanel = new SpringLayout();
 
-		opponentInfoPanelWidth = 200;
-		opponentInfoPanelHeight = 150;
 		setupFrame();
 	}
 
 	private void setupFrame() {
 		this.setContentPane(mainPanel);
-		mainPanel.add(handPanel);
-		mainPanel.add(bfPanel);
 
-		bfPanel.setLayout(sl_bfPanel);
-		bfPanel.add(opponentInfo1);
-		bfPanel.add(opponentInfo2);
-		bfPanel.add(opponentInfo3);
-		bfPanel.add(opponentInfo4);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-		bfPanel.add(ownInfo);
-
-		
-		sl_mainPanel.putConstraint(SpringLayout.NORTH, handPanel, -200, SpringLayout.SOUTH, mainPanel);
-		sl_mainPanel.putConstraint(SpringLayout.SOUTH, handPanel, 0, SpringLayout.SOUTH, mainPanel);
-		sl_mainPanel.putConstraint(SpringLayout.WEST, handPanel, 0, SpringLayout.WEST, mainPanel);
-		sl_mainPanel.putConstraint(SpringLayout.EAST, handPanel, 0, SpringLayout.EAST, mainPanel);
-
-		sl_mainPanel.putConstraint(SpringLayout.NORTH, bfPanel, 0, SpringLayout.NORTH, mainPanel);
-		sl_mainPanel.putConstraint(SpringLayout.WEST, bfPanel, 0, SpringLayout.WEST, handPanel);
-		sl_mainPanel.putConstraint(SpringLayout.SOUTH, bfPanel, 0, SpringLayout.NORTH, handPanel);
-		sl_mainPanel.putConstraint(SpringLayout.EAST, bfPanel, 1000, SpringLayout.WEST, handPanel);
-
-		sl_bfPanel.putConstraint(SpringLayout.WEST, ownInfo, 100, SpringLayout.EAST, opponentInfo1);
-		sl_bfPanel.putConstraint(SpringLayout.NORTH, ownInfo, 200, SpringLayout.NORTH, bfPanel);
-		sl_bfPanel.putConstraint(SpringLayout.SOUTH, ownInfo, 0, SpringLayout.SOUTH, bfPanel);
-		sl_bfPanel.putConstraint(SpringLayout.EAST, ownInfo, -100, SpringLayout.WEST, opponentInfo3);
-
-		sl_bfPanel.putConstraint(SpringLayout.NORTH, opponentInfo1, 0, SpringLayout.NORTH, bfPanel);
-		sl_bfPanel.putConstraint(SpringLayout.WEST, opponentInfo1, 0, SpringLayout.WEST, bfPanel);
-		sl_bfPanel.putConstraint(SpringLayout.EAST, opponentInfo1, opponentInfoPanelWidth, SpringLayout.WEST, bfPanel);
-		sl_bfPanel.putConstraint(SpringLayout.SOUTH, opponentInfo1, opponentInfoPanelHeight, SpringLayout.NORTH, bfPanel);
-
-		sl_bfPanel.putConstraint(SpringLayout.NORTH, opponentInfo2, 0, SpringLayout.NORTH, bfPanel);
-		sl_bfPanel.putConstraint(SpringLayout.SOUTH, opponentInfo2, 0, SpringLayout.SOUTH, opponentInfo1);
-		sl_bfPanel.putConstraint(SpringLayout.EAST, opponentInfo2, 0, SpringLayout.EAST, bfPanel);
-		sl_bfPanel.putConstraint(SpringLayout.WEST, opponentInfo2, -(opponentInfoPanelWidth), SpringLayout.EAST, bfPanel);
-
-		sl_bfPanel.putConstraint(SpringLayout.NORTH, opponentInfo3, -(opponentInfoPanelHeight), SpringLayout.SOUTH, bfPanel);
-		sl_bfPanel.putConstraint(SpringLayout.WEST, opponentInfo3, 0, SpringLayout.WEST, opponentInfo2);
-		sl_bfPanel.putConstraint(SpringLayout.SOUTH, opponentInfo3, 0, SpringLayout.SOUTH, bfPanel);
-		sl_bfPanel.putConstraint(SpringLayout.EAST, opponentInfo3, 0, SpringLayout.EAST, opponentInfo2);
-
-		sl_bfPanel.putConstraint(SpringLayout.NORTH, opponentInfo4, 0, SpringLayout.NORTH, opponentInfo3);
-		sl_bfPanel.putConstraint(SpringLayout.SOUTH, opponentInfo4, 0, SpringLayout.SOUTH, opponentInfo3);
-		sl_bfPanel.putConstraint(SpringLayout.EAST, opponentInfo4, 0, SpringLayout.EAST, opponentInfo1);
-		sl_bfPanel.putConstraint(SpringLayout.WEST, opponentInfo4, 0, SpringLayout.WEST, bfPanel);
 	}
 
 	public void start() {
--- src/gui/GamePanel.java
+++ src/gui/GamePanel.java
@@ -11,16 +11,24 @@ import javax.swing.JTextPane;
 public class GamePanel extends JPanel {
 	private SpringLayout springLayout;
 	private JTextPane textPane;
+	private HandPanel handPanel;
+	private BattlefieldPanel bfPanel;
 
 	public GamePanel() {
 		springLayout = new SpringLayout();
 		textPane = new JTextPane();
+		handPanel = new HandPanel();
+		bfPanel = new BattlefieldPanel();
+
 		setupPanel();
 	}
 
 	private void setupPanel() {
 		setBackground(Color.PINK);
 		setLayout(springLayout);
+		
+		this.add(handPanel);
+		this.add(bfPanel);
 
 		add(textPane);
 		textPane.setBackground(Color.PINK);
@@ -29,8 +37,20 @@ public class GamePanel extends JPanel {
 		springLayout.putConstraint(SpringLayout.WEST, textPane, -198, SpringLayout.EAST, this);
 		springLayout.putConstraint(SpringLayout.SOUTH, textPane, 386, SpringLayout.NORTH, this);
 		springLayout.putConstraint(SpringLayout.EAST, textPane, -10, SpringLayout.EAST, this);
+		
+		springLayout.putConstraint(SpringLayout.NORTH, handPanel, -200, SpringLayout.SOUTH, this);
+		springLayout.putConstraint(SpringLayout.SOUTH, handPanel, 0, SpringLayout.SOUTH, this);
+		springLayout.putConstraint(SpringLayout.WEST, handPanel, 0, SpringLayout.WEST, this);
+		springLayout.putConstraint(SpringLayout.EAST, handPanel, 0, SpringLayout.EAST, this);
+
+		springLayout.putConstraint(SpringLayout.NORTH, bfPanel, 0, SpringLayout.NORTH, this);
+		springLayout.putConstraint(SpringLayout.WEST, bfPanel, 0, SpringLayout.WEST, handPanel);
+		springLayout.putConstraint(SpringLayout.SOUTH, bfPanel, 0, SpringLayout.NORTH, handPanel);
+		springLayout.putConstraint(SpringLayout.EAST, bfPanel, -300, SpringLayout.EAST, handPanel);
 
 		textPane.setText(
 				"Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.");
+	
+	
 	}
 }
