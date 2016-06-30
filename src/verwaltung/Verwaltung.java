c1c9638dd3ffa2b1f9b54dccfc87557c2166e7cd
Michael <Michael@Michael-PC.Speedport_W_921V_1_39_000>
2016-06-30 10:15:26 +0200

    Verwaltung java hinzugefügt dummy hülle

---
+++ src/verwaltung/Verwaltung.java
@@ -0,0 +1,18 @@
+package verwaltung;
+
+public class Verwaltung {
+	private int anzahlSpieler;
+	private int aktuelleSpielphase;
+	
+	public int pruefeAnazhlDerHandkarten(){
+		return 0;
+	}
+	
+	public Object ermittlePiratenKoenig(){
+		return new Object();
+	}
+	
+	public void initKartenStapel(){
+		
+	}
+}
