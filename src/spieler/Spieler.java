1ded552e9d37c671ce7e4afda08cc01a79f246e6
nowak <nowak@172.22.211.60>
2016-07-05 15:03:38 +0200

    Spieler angepasst

--- src/spieler/Spieler.java
+++ src/spieler/Spieler.java
@@ -1,5 +1,7 @@
 package spieler;
 
+import java.util.Vector;
+
 import Karten.SWT2.src.Flaute;
 import Karten.SWT2.src.Karte;
 
@@ -11,12 +13,35 @@ public class Spieler {
 	private int dublonen;
 	private int siegpunkte;
 	private int anzahlSchiffe;
-	private Karte handkarten[];
+	private Vector<Karte> handkarten;
+	
+	
+	public Spieler (int id, String name){
+		this.id = id;
+		this.name = name;
+		kanonen = new int[4];
+		dublonen= 0;
+		siegpunkte = 0;
+		anzahlSchiffe =0;
+		handkarten = new Vector<Karte>();
+		
+	};
 	
 	private void richteKanone(Spieler ziel){
+		int i =0;
+		boolean kanoneGesetzt=false;
+		while(i<kanonen.length&& !kanoneGesetzt){
+			if(kanonen[i]==0){
+				kanonen[i] = ziel.getId();
+				kanoneGesetzt= true;
+			}
+			i++;
+		}
+		
 	
 	};
 	
+	public int getId(){return id;};
 	public void spieleKarteAus(Karte karte){};
 	public void spieleKarteAus(Karte karte, Spieler spieler){};
 	public Karte macheBeute(Spieler spieler){ return new Flaute();};
